e036492c778f745403bdb5384067b20e5fcdc5b8
rayenbmv2 <rayenbm69@gmail.com>
2026-07-08 09:22:46 +0100

    Update promotions.ts

--- src/lib/promotions.ts
+++ src/lib/promotions.ts
@@ -17,7 +17,7 @@ export const PROMOTIONS: Promotion[] = [
     size: "2 L",
     packQty: 3,
     priceTnd: 12.0,
-    oldPriceTnd: 12.9,
+    oldPriceTnd: 4.3,
     image: productWater,
   },
   {
@@ -35,7 +35,7 @@ export const PROMOTIONS: Promotion[] = [
     size: "2 L",
     packQty: 3,
     priceTnd: 12.0,
-    oldPriceTnd: 12.9,
+    oldPriceTnd: 4.3,
     image: productWater,
   },
 ];
